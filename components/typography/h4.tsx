62e26894fb60c91b147b5028f70b3d6912479d10
Swinkels, Luc L <l.swinkels-ad@student.fontys.nl>
2023-09-11 23:06:54 +0200

    add typography

---
+++ components/typography/h4.tsx
@@ -0,0 +1,7 @@
+export function H4({ children }: { children: React.ReactNode }) {
+  return (
+    <h3 className="scroll-m-20 text-xl font-semibold tracking-tight">
+      {children}
+    </h3>
+  )
+}
