b1d02894829f835cd2dbcb21ef513b934ac6df7c
Swinkels, Luc L <l.swinkels-ad@student.fontys.nl>
2023-09-18 15:37:06 +0200

    update product structure

--- app/projects/individual-project/portfolio-c4-model/page.tsx
+++ app/projects/individual-project/portfolio-c4-model/page.tsx
@@ -20,24 +20,7 @@ export default function PortfolioC4Model() {
 
   const Content = () => {
     return (
-      <>
-        <ContentSection title="Lorem Ipsum">
-          <P>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci
-            tenetur excepturi quam. Nulla minus quaerat facere perspiciatis
-            numquam ratione eligendi ipsa aliquid cum officia possimus quas iste
-            nam, suscipit quisquam. Dolorum facere quaerat explicabo, cupiditate
-            eligendi eveniet, nostrum, quisquam voluptatum eos ipsam ducimus
-            cumque eaque nulla voluptatibus! Ea, vitae soluta?
-          </P>
-          <Image
-            src="/logo/banner.png"
-            alt="lorem"
-            className="rounded-lg border shadow-sm mt-8 lg:mt-16 xl:mt-32"
-            width={1920}
-            height={1080}
-          />
-        </ContentSection>
+      <div className="space-y-16 xl:space-y-32">
         <ContentSection title="Lorem Ipsum">
           <P>
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci
@@ -48,7 +31,24 @@ export default function PortfolioC4Model() {
             cumque eaque nulla voluptatibus! Ea, vitae soluta?
           </P>
         </ContentSection>
-      </>
+        <Image
+          src="/logo/banner.png"
+          alt="lorem"
+          className="rounded-lg border shadow-sm"
+          width={1920}
+          height={1080}
+        />
+        <ContentSection title="Lorem Ipsum">
+          <P>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci
+            tenetur excepturi quam. Nulla minus quaerat facere perspiciatis
+            numquam ratione eligendi ipsa aliquid cum officia possimus quas iste
+            nam, suscipit quisquam. Dolorum facere quaerat explicabo, cupiditate
+            eligendi eveniet, nostrum, quisquam voluptatum eos ipsam ducimus
+            cumque eaque nulla voluptatibus! Ea, vitae soluta?
+          </P>
+        </ContentSection>
+      </div>
     );
   };
 
--- components/product-details.tsx
+++ components/product-details.tsx
@@ -8,29 +8,33 @@ import { Product } from "@/lib/types/Product";
 
 import { H1 } from "./typography/h1";
 import { H4 } from "./typography/h4";
-import { MutedText } from "./typography/muted-text";
 import { P } from "./typography/p";
 import { Badge } from "./ui/badge";
 import { Card } from "./ui/card";
 
 export default function ProductDetails({ product }: { product: Product }) {
   return (
-    <>
-      <div className="grid 2xl:grid-cols-3 gap-8 2xl:gap-2 mb-8 xl:mb-16">
-        <div className="2xl:col-span-2 w-full">
-          {/* <MutedText className="mb-4">{product.project.title}</MutedText> */}
-          <div className="flex gap-2 mb-4 flex-wrap">
-            {product.categories.map((category, i) => (
-              <Badge key={i} variant={category} className="capitalize">
-                {category}
-              </Badge>
-            ))}
-          </div>
-          <H1>{product.title}</H1>
-          <P>{product.description}</P>
-        </div>
-        {product.data && (
-          <Card className="flex flex-col gap-8 p-0 shadow-none md:shadow-sm border-0 md:border md:p-6 lg:p-8 md:w-max 2xl:ml-auto w-full h-max">
+    <div className="mb-16 xl:mb-32">
+      <div className="flex gap-2 mb-4 flex-wrap">
+        {product.categories.map((category, i) => (
+          <Badge key={i} variant={category} className="capitalize">
+            {category}
+          </Badge>
+        ))}
+      </div>
+      <H1>{product.title}</H1>
+      <P>{product.description}</P>
+      <Image
+        src={product.img}
+        alt={product.title}
+        className="rounded-lg border shadow-sm my-16"
+        width={1920}
+        height={1080}
+        priority
+      />
+      {product.data && (
+        <div className="grid xl:grid-cols-2 gap-8">
+          <Card className="flex flex-col gap-8 p-0 shadow-none md:shadow-sm border-0 md:border md:p-6 lg:p-8 w-full">
             {product.data.learningOutcomes && (
               <div>
                 <H4>Learning Outcomes</H4>
@@ -43,6 +47,8 @@ export default function ProductDetails({ product }: { product: Product }) {
                 </div>
               </div>
             )}
+          </Card>
+          <Card className="flex flex-col gap-8 p-0 shadow-none md:shadow-sm border-0 md:border md:p-6 lg:p-8 w-full">
             {product.data.researchMethods && (
               <div>
                 <H4>Research Methods</H4>
@@ -56,16 +62,8 @@ export default function ProductDetails({ product }: { product: Product }) {
               </div>
             )}
           </Card>
-        )}
-      </div>
-      <Image
-        src={product.img}
-        alt={product.title}
-        className="rounded-lg border shadow-sm"
-        width={1920}
-        height={1080}
-        priority
-      />
-    </>
+        </div>
+      )}
+    </div>
   );
 }
