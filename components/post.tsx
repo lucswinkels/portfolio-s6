1033cd989fbd571f9b2a3fa37e67ee43cc0ec844
Swinkels, Luc L <l.swinkels-ad@student.fontys.nl>
2023-10-30 19:44:30 +0100

    small styling changes

--- components/post.tsx
+++ components/post.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { client } from "@/sanity/lib/client";
 import { PortableText } from "@portabletext/react";
+import { getImageDimensions } from "@sanity/asset-utils";
 import { SanityDocument } from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
 import { ChevronRight } from "lucide-react";
@@ -13,6 +14,7 @@ import { slugify } from "@/lib/utils";
 import FadeUp from "./animation/fade-up";
 import Container from "./container";
 import { H1 } from "./typography/h1";
+import { MutedText } from "./typography/muted-text";
 import { P } from "./typography/p";
 import { Badge } from "./ui/badge";
 import { Button } from "./ui/button";
@@ -20,23 +22,43 @@ import { Button } from "./ui/button";
 const builder = imageUrlBuilder(client);
 
 export default function Post({ post }: { post: SanityDocument }) {
+  const ImageComponent = ({ value }: any) => {
+    const { width, height } = getImageDimensions(value);
+    return (
+      <div className="text-center my-16">
+        <Image
+          src={builder.image(value).width(width).height(height).url()}
+          alt={value.alt}
+          width={width}
+          height={height}
+          loading="lazy"
+          className="rounded-lg border mb-2"
+        />
+        <MutedText className="m-0 italic">{value.alt}</MutedText>
+      </div>
+    );
+  };
+  const components = {
+    types: {
+      image: ImageComponent,
+    },
+  };
+
   return (
     <FadeUp>
       <Container>
         <div className="md:flex hidden items-center mb-4">
-          <Button variant="link" className="p-0">
+          <small className="text-sm leading-none text-muted-foreground">
             <Link href="/">Home</Link>
-          </Button>
+          </small>
           <ChevronRight className="mx-1 h-4 w-4" />
-          <Button variant="link" className="p-0">
+          <small className="text-sm leading-none text-muted-foreground">
             <Link href={`/projects/${slugify(post.project)}`}>
               {post.project}
             </Link>
-          </Button>
+          </small>
           <ChevronRight className="mx-1 h-4 w-4" />
-          <Button variant="link" disabled className="p-0">
-            <Link href="/">{post.title}</Link>
-          </Button>
+          <small className="text-sm font-medium">{post.title}</small>
         </div>
         <H1>{post.title}</H1>
         <P>{post.description}</P>
@@ -58,14 +80,14 @@ export default function Post({ post }: { post: SanityDocument }) {
         ) : null}
         <Image
           src={builder.image(post.mainImage).width(1920).height(1080).url()}
-          className="rounded-lg mb-16 border"
+          className="rounded-lg mb-16 xl:mb-32 border"
           width={1920}
           height={1080}
           alt={post.mainImage.alt}
           priority
         />
         <div className="prose dark:prose-invert">
-          <PortableText value={post.body} />
+          <PortableText value={post.body} components={components} />
         </div>
       </Container>
     </FadeUp>
