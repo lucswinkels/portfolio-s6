2f259b9d0adda69a5f43fd8596bc37fde60d8c2a
Swinkels, Luc L <l.swinkels-ad@student.fontys.nl>
2023-12-19 21:10:18 +0100

    add text-balance to h2/h3

--- components/typography/h3.tsx
+++ components/typography/h3.tsx
@@ -12,7 +12,7 @@ export function H3({
   return (
     <h3
       className={cn(
-        "scroll-m-20 text-2xl font-semibold tracking-tight",
+        "scroll-m-20 text-2xl font-semibold tracking-tight text-balance",
         className
       )}
     >
